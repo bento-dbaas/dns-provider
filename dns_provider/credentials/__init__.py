06d19118ca282356c862159cdea08daeaf4203be
Eliakin Costa <eliakim170@gmail.com>
2019-06-25 18:07:19 -0300

    credentials package improvements
     -  Docs updated
     -  credentials are not hard coded anymore

--- dns_provider/credentials/__init__.py
+++ dns_provider/credentials/__init__.py
@@ -1,3 +1,3 @@
-""" credentials is the module where all of the settings to access each environment
-are present (e.g. dev and pro environments).
+""" credentials is the module where all of the settings and methods to access
+each environment are present (e.g. dev and prod environments).
 """
--- dns_provider/credentials/dev.py
+++ dns_provider/credentials/dev.py
@@ -1,9 +1,20 @@
+from dns_provider import settings
+
 class Credential(object):
+    """This class holds the credentials and methods to work on dev env."""
 
     def __init__(self, environment):
-        self.user='dbproducao@corp.globo.com'
-        self.password='123456'
-        self.endpoint = 'http://int.gdns.qa.globoi.com'
+        self.user = settings.DNS_PROVIDER_GDNS_USER
+        self.password = settings.DNS_PROVIDER_GDNS_PWD
+        self.endpoint = settings.DNS_PROVIDER_GDNS_ENDPOINT
 
     def get_credentials(self, environment):
+        """This method returns the credential to a given environment.
+
+        Parameters:
+        environment (str): dev or prod
+
+        Returns:
+        credential (Credential): The Credential object itself
+        """
         return self
