44aa4790364bf12aae7bb95ed9e331544a42c8a2
Eliakin Costa <eliakim170@gmail.com>
2019-06-25 18:11:07 -0300

    providers package improvements
    - Updated code to python3
    - Documentation to all methods and DNSAPI class
    - delete_record implemented
    - removed unused imports

--- dns_provider/providers/__init__.py
+++ dns_provider/providers/__init__.py
@@ -1,3 +1,4 @@
 """ In providers we have the implementation of the most important primitives for
 each specific provider. Regarding DNS, 'create_dns' is a good example of primitive.
+For instance we have GDNS as the only implementation available.
 """
