246e8eabaaa672217b29fd6a599b69f142a98391
Eliakin Costa <eliakim170@gmail.com>
2019-06-26 09:24:38 -0300

    New feature - Delete DNS Record
    -  delete_dns endpoint created
    -  Tests for basic flow
    -  Documentation: Implementation and Tests

--- dns_provider/main.py
+++ dns_provider/main.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from flask_httpauth import HTTPBasicAuth
 
-from providers.gdns import DNSAPI
+from dns_provider.providers.gdns import DNSAPI
 
 
 app = Flask(__name__)
@@ -14,6 +14,18 @@ cors = CORS(app)
 
 @app.route("/dns/", methods=['POST'])
 def create_dns():
+    """This function exposes the uri /dns/. That endpoint creates a DNS on GDNS
+    provider. The HTTP methods accepted are: ['POST'].
+
+    Payload Parameters:
+    name (str): DNS name
+    domain (str): domain name
+    environment (str): environment
+    ip (str): IP address
+
+    Returns:
+    response (JSON): A JSON response and the status code
+    """
     data = request.get_json()
     dns = '{}.{}'.format(data.get('name'), data.get('domain'))
 
@@ -32,3 +44,32 @@ def create_dns():
     dnsapi.create_record(data.get('name'), data.get('ip'), domain_id=domain_id)
 
     return jsonify(data=dict(message="DNS '{}' successfully created.".format(dns))), 201
+
+@app.route("/dns/<string:name>/<string:domain>", methods=['DELETE'])
+def delete_dns(name, domain):
+    """This function exposes the uri /dns/<name>/<domain>. That endpoint deleted
+    a DNS on GDNS provider given the proper parameters. The HTTP methods accepted
+    are: ['Delete'].
+
+    Path Parameters:
+    name (str): DNS name
+    domain (str): domain name
+
+    Returns:
+    response (JSON): A JSON response and the status code
+    """
+    dnsapi = DNSAPI('dev')
+
+    domain_id = dnsapi.get_domain_id_by_name(domain=domain)
+    if not domain_id:
+        error = "Domain '{}' not found!".format(domain)
+        return jsonify({'result': error }), 404
+
+    record_id = dnsapi.get_record_by_name(name, domain_id=domain_id)
+    if not record_id:
+        error = "Name '{}' not found!".format(name)
+        return jsonify({'result': error }), 404
+
+    dnsapi.delete_record(record_id)
+
+    return jsonify({}), 204
