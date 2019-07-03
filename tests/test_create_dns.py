5f0d5fed1b2c8ea3ce1280b5c33e7e8be80364b4
Eliakin Costa <eliakim170@gmail.com>
2019-07-03 16:03:26 -0300

    Improving Tests to create_dns - Mocks for GDNS
    -  The tests are not directly calling the GDNS anymore

--- tests/test_create_dns.py
+++ tests/test_create_dns.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch
 import json
 
 from dns_provider.main import app
@@ -11,11 +12,14 @@ class ServiceCreateDNSTestCase(TestCase):
         self.client = app.test_client()
         self.client.testing = True
 
-    def test_create_dns_success(self):
+    @patch('dns_provider.providers.gdns.DNSAPI.create_record')
+    def test_create_dns_success(self, mock_create_record):
         """It tests the dns creation through '/dns/' path and POST method."""
+        success_msg = "DNS 'test-dns-provider.dev.globoi.com' successfully created."
+        mock_create_record.return_value = 'test-dns-provider.dev.globoi.com'
         response = self.client.post('/dns/',
                                     data=json.dumps({
-                                        'name':'test-dns-provider3',
+                                        'name':'test-dns-provider',
                                         'domain': 'dev.globoi.com',
                                         'environment': 'dev',
                                         'ip': '10.224.77.89'
@@ -23,11 +27,15 @@ class ServiceCreateDNSTestCase(TestCase):
                                     content_type='application/json')
 
         self.assertEqual(response.status_code, 201)
-        self.assertIn(dict(data="DNS 'test-dns-provider3.dev.globoi.com' successfully created."), json.loads(response.data))
+        self.assertEqual(dict(data=dict(message=success_msg)),
+                         json.loads(response.data))
 
-    def test_create_dns_invalid_domain(self):
+    @patch('dns_provider.providers.gdns.DNSAPI.get_domain_id_by_name')
+    def test_create_dns_invalid_domain(self, mock_get_domain_id_by_name):
         """It tests the dns creation with an invalid domain."""
+        mock_get_domain_id_by_name.return_value = None
         error_msg = "Domain 'invalid_domain_test' not found!"
+
         response = self.client.post('/dns/',
                                     data=json.dumps({
                                         'name':'test_dns_provider',
@@ -40,12 +48,19 @@ class ServiceCreateDNSTestCase(TestCase):
         self.assertEqual(response.status_code, 404)
         self.assertEqual(dict(error=error_msg), json.loads(response.data))
 
-    def test_create_dns_name_already_exists(self):
+    @patch('dns_provider.providers.gdns.DNSAPI.get_record_by_name')
+    @patch('dns_provider.providers.gdns.DNSAPI.get_domain_id_by_name')
+    def test_create_dns_name_already_exists(self,
+                                            mock_get_domain_id_by_name,
+                                            mock_get_record_by_name):
         """It tests the dns creation with a name already exists."""
-        error_msg = "Could not create dns 'dbaas.dev.globoi.com', it already exists!"
+        error_msg = "Could not create dns 'test-exist.dev.globoi.com', it already exists!"
+        mock_get_domain_id_by_name.return_value = 99999
+        mock_get_record_by_name.return_value = 99998
+
         response = self.client.post('/dns/',
                                     data=json.dumps({
-                                        'name':'dbaas',
+                                        'name':'test-exist',
                                         'domain': 'dev.globoi.com',
                                         'environment': 'dev',
                                         'ip': '10.224.77.89'
