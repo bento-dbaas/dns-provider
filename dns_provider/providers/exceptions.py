5fb48812926db96eab240ca4606cb915317131aa
Eliakin Costa <eliakim170@gmail.com>
2019-07-08 18:05:09 -0300

    Added JSONException to the project
    -  This class deals with exceptions raised during endpoint calls and convert them to the json:api pattern

--- dns_provider/providers/exceptions.py
+++ dns_provider/providers/exceptions.py
@@ -24,3 +24,16 @@ class DNSUnknownError(Exception):
 
     def __str__(self):
         return 'DNSUnknownError({0}, http={1})'.format(str(self.path), self.status)
+
+class JSONException(Exception):
+
+    def __init__(self, message, status_code, payload=None):
+        Exception.__init__(self)
+        self.status_code = status_code
+        self.error_dict = dict(message=message, code=self.status_code)
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['error'] = self.error_dict
+        return rv
