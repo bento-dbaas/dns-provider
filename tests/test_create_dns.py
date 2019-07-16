0580f86ec715737407e9455aa61d451f77fc4789
Eliakin Costa <eliakim170@gmail.com>
2019-07-16 14:30:51 -0300

    Tests refactoring - mongomock
    - Testing mongo insertions and deletions
    - Better reuse for variables
    - Documentation

--- tests/test_create_dns.py
+++ tests/test_create_dns.py
@@ -1,7 +1,11 @@
 from unittest import TestCase
 from unittest.mock import patch
 import json
+import collections
 
+from mongoengine import connect, disconnect
+
+from dns_provider import models
 from dns_provider.main import app
 
 
@@ -10,6 +14,14 @@ from dns_provider.main import app
 @patch('dns_provider.providers.gdns.DNSAPI.create_record')
 class ServiceCreateDNSTestCase(TestCase):
     """This class executes test for create_dns view."""
+
+    @classmethod
+    def setUpClass(cls):
+        connect('mongoenginetest', host='mongomock://localhost')
+
+    @classmethod
+    def tearDownClass(cls):
+        disconnect()
 
     def setUp(self):
         self.client = app.test_client()
@@ -20,23 +32,29 @@ class ServiceCreateDNSTestCase(TestCase):
                                 mock_get_domain_id_by_name,
                                 mock_get_record_by_name):
         """It tests the dns creation through '/dns/' path and POST method."""
-        success_msg = "DNS 'test-dns-provider.dev.globoi.com' successfully created."
+        success_status_code = 201
+        name = 'test-dns-provider'
+        domain = 'dev.globoi.com'
+        ip = '10.224.77.89'
+        fake_data = dict(name=name, domain=domain, ip=ip)
+        fake_response = dict(data=fake_data)
         mock_create_record.return_value = 'test-dns-provider.dev.globoi.com'
         mock_get_domain_id_by_name.return_value  = 99999
         mock_get_record_by_name.return_value = None
 
         response = self.client.post('/dns/',
-                                    data=json.dumps({
-                                        'name':'test-dns-provider',
-                                        'domain': 'dev.globoi.com',
-                                        'environment': 'dev',
-                                        'ip': '10.224.77.89'
-                                    }),
+                                    data=json.dumps(fake_data),
                                     content_type='application/json')
 
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(dict(data=dict(message=success_msg)),
-                         json.loads(response.data.decode('utf-8')))
+        dns_object = models.DNS.objects.filter(
+            name=name,
+            domain=domain
+        ).first()
+
+        self.assertEqual(response.status_code, success_status_code)
+        self.assertEqual(fake_response,
+                         response.json)
+        self.assertEqual(len(models.DNS.objects.all()), 1)
 
     def test_create_dns_invalid_domain(self,
                                        mock_create_record,
@@ -45,18 +63,20 @@ class ServiceCreateDNSTestCase(TestCase):
         """It tests the dns creation with an invalid domain."""
         mock_get_domain_id_by_name.return_value = None
         error_msg = "Domain 'invalid_domain_test' not found!"
+        error_status_code = 404
+        fake_response = dict(error=dict(code=error_status_code, message=error_msg))
 
         response = self.client.post('/dns/',
                                     data=json.dumps({
                                         'name':'test_dns_provider',
                                         'domain': 'invalid_domain_test',
-                                        'environment': 'dev',
                                         'ip': '10.224.77.89'
                                     }),
                                     content_type='application/json')
 
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(dict(error=error_msg), json.loads(response.data.decode('utf-8')))
+        self.assertEqual(len(models.DNS.objects.all()), 0)
+        self.assertEqual(fake_response, response.json)
 
     def test_create_dns_name_already_exists(self,
                                             mock_create_record,
@@ -64,6 +84,8 @@ class ServiceCreateDNSTestCase(TestCase):
                                             mock_get_record_by_name):
         """It tests the dns creation with a name already exists."""
         error_msg = "Could not create dns 'test-exist.dev.globoi.com', it already exists!"
+        error_status_code = 422
+        fake_response = dict(error=dict(code=error_status_code, message=error_msg))
         mock_get_domain_id_by_name.return_value = 99999
         mock_get_record_by_name.return_value = 99998
 
@@ -71,10 +93,10 @@ class ServiceCreateDNSTestCase(TestCase):
                                     data=json.dumps({
                                         'name':'test-exist',
                                         'domain': 'dev.globoi.com',
-                                        'environment': 'dev',
                                         'ip': '10.224.77.89'
                                     }),
                                     content_type='application/json')
 
         self.assertEqual(response.status_code, 422)
-        self.assertEqual(dict(error=error_msg), json.loads(response.data.decode('utf-8')))
+        self.assertEqual(len(models.DNS.objects.all()), 0)
+        self.assertEqual(fake_response, response.json)
--- tests/test_delete_dns.py
+++ tests/test_delete_dns.py
@@ -2,7 +2,10 @@ from unittest import TestCase
 import json
 from unittest.mock import patch
 
+from mongoengine import connect, disconnect
+
 from dns_provider.main import app
+from dns_provider import models
 
 
 @patch('dns_provider.providers.gdns.DNSAPI.get_record_by_name')
@@ -11,9 +14,30 @@ from dns_provider.main import app
 class ServiceDeleteDNSTestCase(TestCase):
     """This class executes test for delete_dns view."""
 
+    @classmethod
+    def setUpClass(cls):
+        connect('mongoenginetest', host='mongomock://localhost')
+
+    @classmethod
+    def tearDownClass(cls):
+        disconnect()
+
     def setUp(self):
         self.client = app.test_client()
         self.client.testing = True
+        self.fake_name = 'test-dns-provider'
+        self.fake_domain = 'dev.globoi.com'
+        self.fake_ip = '10.224.77.89'
+        self.fake_data = dict(
+            name=self.fake_name,
+            domain=self.fake_domain,
+            ip=self.fake_ip
+        )
+        dns = models.DNS(**self.fake_data)
+        dns.save()
+
+    def tearDown(self):
+        models.DNS.objects().delete()
 
     def test_delete_dns_success(self,
                                 mock_delete_record,
@@ -23,11 +47,11 @@ class ServiceDeleteDNSTestCase(TestCase):
         mock_delete_record.return_value = None
         mock_get_domain_id_by_name.return_value = 99998
         mock_get_record_by_name.return_value = 99999
-
-        response = self.client.delete('/dns/test-dns-provider/dev.globoi.com', content_type='application/json')
+        url = '/dns/{}/{}'.format(self.fake_name, self.fake_domain)
+        response = self.client.delete(url, content_type='application/json')
 
         self.assertEqual(response.status_code, 204)
-        self.assertEqual({}, dict(response.data))
+        self.assertEqual(len(models.DNS.objects.all()), 0)
 
     def test_delete_dns_invalid_domain(self,
                                        mock_delete_record,
@@ -35,13 +59,15 @@ class ServiceDeleteDNSTestCase(TestCase):
                                        mock_get_record_by_name):
         """It tests the dns creation with an invalid domain."""
         mock_get_domain_id_by_name.return_value = None
-        error_msg = "Domain 'invalid_domain_test' not found!"
+        error_msg = "Domain '{}' not found!".format(self.fake_domain)
+        error_status_code = 404
+        fake_error_response = dict(error=dict(message=error_msg, code=error_status_code))
 
-        response = self.client.delete('/dns/test-dns-provider/invalid_domain_test', content_type='application/json')
+        response = self.client.delete('/dns/{}/{}'.format(self.fake_name, self.fake_domain), content_type='application/json')
 
         self.assertEqual(response.status_code, 404)
-        print(response.data)
-        self.assertEqual(dict(error=error_msg), json.loads(response.data.decode('utf-8')))
+        self.assertEqual(fake_error_response, response.json)
+        self.assertEqual(len(models.DNS.objects.all()), 1)
 
     def test_delete_dns_invalid_name(self,
                                      mock_delete_record,
@@ -50,9 +76,12 @@ class ServiceDeleteDNSTestCase(TestCase):
         """It tests the dns creation with a name already exists."""
         mock_get_domain_id_by_name.return_value = 99998
         mock_get_record_by_name.return_value = None
-        error_msg = "Name 'invalid_dns_name' not found!"
+        error_msg = "Name '{}' not found!".format(self.fake_name)
+        error_status_code = 404
+        fake_error_response = dict(error=dict(message=error_msg, code=error_status_code))
 
-        response = self.client.delete('/dns/invalid_dns_name/dev.globoi.com', content_type='application/json')
+        response = self.client.delete('/dns/{}/{}'.format(self.fake_name, self.fake_domain), content_type='application/json')
 
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(dict(error=error_msg), json.loads(response.data.decode('utf-8')))
+        self.assertEqual(fake_error_response, response.json)
+        self.assertEqual(len(models.DNS.objects.all()), 1)
